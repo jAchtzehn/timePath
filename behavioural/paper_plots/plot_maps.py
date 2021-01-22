711d43290f4d27cfcc8330e936f0c4e4a02a15fb
jAchtzehn <johannes.achtzehn@gmail.com>
2021-01-22 15:24:58 +0100

    Whole brain searchlight RSA added

--- behavioural/paper_plots/plot_maps.py
+++ behavioural/paper_plots/plot_maps.py
@@ -1,4 +1,4 @@
-from nilearn.plotting import plot_stat_map
+from nilearn.plotting import plot_stat_map, plot_glass_brain
 from os.path import join as opj
 from os.path import abspath
 import matplotlib.pyplot as plt
@@ -7,19 +7,27 @@ from nilearn._utils.niimg import _safe_get_data
 
 import numpy as np
 
-stat_img_fname = abspath('/home/achtzehnj/data/timePath/conjAll_thrSPM.nii')
-atlas_img = abspath('/home/achtzehnj/data/atlases/100um/Synthesized_FLASH25_in_MNI_v2_200um.nii.gz')
-atlas_img_500 = abspath('/home/achtzehnj/data/atlases/100um/Synthesized_FLASH25_in_MNI_v2_500um.nii.gz')
-
+stat_img_fname = abspath('/Users/jachtzehn/Documents/DZNE/timePath/paper/figures/fig3/conjAll_thrSPM.nii')
+atlas_img = abspath('/Users/jachtzehn/data/fMRI/atlases/100um/Synthesized_FLASH25_in_MNI_v2_200um.nii.gz')
 
 stat_img = load_img(stat_img_fname)
 stat_img_ = _safe_get_data(stat_img, ensure_finite=True)
 
 stat_img__ = new_img_like(stat_img, data=stat_img_)
-stat_img_resampled = resample_to_img(stat_img__, atlas_img_500)
-stat_img_resampled.to_filename('/home/achtzehnj/data/timePath/conjAll_thrSPM_resampled.nii')
+#stat_img_resampled = resample_to_img(stat_img__, atlas_img_500)
+#stat_img_resampled.to_filename('/home/achtzehnj/data/timePath/conjAll_thrSPM_resampled.nii')
 
-plot_stat_map(stat_img_resampled, cut_coords=[42, 45, -13], draw_cross=False, bg_img=atlas_img_500,
-              cmap='viridis', vmin=5.1, threshold=0, vmax=11.28, colorbar=True, dim=0)
+plot_stat_map(stat_img, black_bg=False, cut_coords=[42, 45, -13], draw_cross=False, bg_img=atlas_img,
+              cmap='plasma', vmin=5.1, threshold=0, vmax=11.28, colorbar=True, dim=0)
 
-plt.savefig('/home/achtzehnj/Downloads/conj_resampled.pdf', dpi=500, format='pdf')
+plt.savefig('/Users/jachtzehn/Documents/DZNE/timePath/paper/figures/fig3/conj_white_bg.pdf', dpi=500, format='pdf')
+plt.close()
+
+# for fname in ['dotsvslumin', 'distvslumin', 'timevslumin', 'conjAll']:
+#
+#     stat_img_folder = abspath('/Users/jachtzehn/Documents/DZNE/timePath/paper/figures/fig3/')
+#     stat_img_fname = opj(stat_img_folder, fname + '_thrSPM.nii')
+#
+#     plot_glass_brain(stat_img_fname, black_bg=False, cmap='plasma', vmin=5.1, threshold=0, vmax=11.28, colorbar=False)
+#     plt.savefig(opj(stat_img_folder, fname + '_gb.png'), dpi=500, format='png')
+#     plt.close()
---
+++ mri/rsa/rsa_searchlight_wholeBrain.py
@@ -0,0 +1,106 @@
+from os.path import join as opj
+from os.path import abspath
+from sys import platform
+import os
+from scipy.spatial.distance import squareform
+from mvpa2.suite import fmri_dataset, zscore, mean_group_sample, remove_nonfinite_features
+from mvpa2.measures import rsa
+from mvpa2.measures.searchlight import sphere_searchlight
+from mvpa2.datasets.mri import map2nifti
+from mvpa2.base.learner import ChainLearner
+from mvpa2.mappers.shape import TransposeMapper
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import numpy as np
+from nilearn.plotting import plot_stat_map
+from nilearn.image.resampling import coord_transform
+from utilFunctions import create_html_report
+import cPickle
+
+# ------------ File I/O ------------
+if platform == 'darwin':
+    experiment_dir = abspath('/Volumes/Seagate/Backups/16102020/data/timePath')
+else:
+    experiment_dir = abspath('/home/achtzehnj/data/timePath/')
+
+nilearn_dir = opj(experiment_dir, 'derivatives', 'nilearn')
+rsa_dir = opj(experiment_dir, 'derivatives', 'rsa')
+
+os.system('mkdir -p %s' % rsa_dir)                 # create the mvpa2 folder for data storage
+
+# ------------ options ------------
+subjects = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]     # subjects to plot
+# [4, 6, 7, 9, 12, 14, 17, 19, 23, 24]                                                                  # 10 best subjects (TSNR >= 50)
+# [1, 2, 3, 5, 8, 10, 11, 13, 15, 16, 18, 20, 21, 22, 25]                                               # the rest
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]               # all except 15
+conditions = ['time', 'dist', 'dots']
+mask = 'wb'
+file_suffix = ''
+plot_html = True
+group_level = True
+save_figs = True
+sl_radius = 6
+spaces = ['MNI152NLin2009cAsym']
+
+os.system('clear')                              # clean up terminal
+# if file_suffix is empty, add _
+if file_suffix == '':
+    file_suffix = '_'
+
+for space in spaces:
+    print('Calculating space: %s' % space)
+
+    for subj in subjects:
+        subj_str = 'sub-' + str(subj).zfill(2)      # create the subject string (just for easier handling)
+
+        print('Subject: %s' % subj_str)
+        result_dir = opj(rsa_dir, subj_str, 'space-' + space, 'results')
+        os.system('mkdir -p %s' % result_dir)                 # create the mvpa2 folder for data storage
+
+        # -- data loading
+        data_path = opj(nilearn_dir, subj_str, 'space-' + space, 'betas', subj_str + '_betas_merged.nii.gz')
+        behav_path = opj(nilearn_dir, subj_str, 'space-' + space, 'betas', subj_str + '_merged_events.tsv')
+
+        mask_path = opj(nilearn_dir, subj_str, 'space-' + space, 'masks',
+                        subj_str + '_' + mask + '_mask_binarized.nii.gz')  # mask filename
+
+        # read in behav data
+        behav_data = pd.read_csv(behav_path, delimiter='\t')
+        targets = behav_data['trial'].tolist()                  # labels are called targets in mvpa
+        chunks = behav_data['class_chunk'].tolist()             # chunks are runpairs of 2 runs
+        runs = behav_data['run'].tolist()
+
+        # create dataset
+        fmri_data = fmri_dataset(data_path, mask=mask_path, targets=targets, chunks=chunks)
+
+
+        fmri_data.sa['runs'] = runs
+        fmri_data = remove_nonfinite_features(fmri_data)
+
+        # -- preprocessing
+        #zscore(fmri_data, chunks_attr='chunks')                     # zscoring the data to remove mean
+        fmri_data = fmri_data[fmri_data.sa.targets != 'cross']      # remove cross from the conditions (targets)
+
+        for cond in ['time', 'dist', 'dots', 'lumin']:
+            if cond not in conditions:
+                fmri_data = fmri_data[fmri_data.sa.targets != cond]      # remove cross from the conditions (targets)
+
+        # -- computation
+        # first compute RDMs
+        mtgs = mean_group_sample(['targets'])       # create the mean of all trials
+        mtds = mtgs(fmri_data)
+        dsm = rsa.PDist(square=False, center_data=True)
+
+        sl = sphere_searchlight(dsm, radius=sl_radius, nproc=24)
+        slres = sl(mtds)
+
+        rdm_data_new_order = np.zeros(shape=slres.samples.shape)
+        rdm_data_new_order[0, :] = slres.samples[1, :]
+        rdm_data_new_order[1, :] = slres.samples[2, :]
+        rdm_data_new_order[2, :] = slres.samples[0, :]
+
+        niimg = map2nifti(fmri_data, rdm_data_new_order)
+
+        niimg.to_filename(opj(result_dir,
+                              subj_str + '_space-' + space + file_suffix + 'wb_rdm_values.nii.gz'))
