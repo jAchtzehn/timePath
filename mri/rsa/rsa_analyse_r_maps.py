55862dd70ff62607cbac5b64f85a2156a30c06d9
jAchtzehn <johannes.achtzehn@gmail.com>
2021-01-29 17:17:06 +0100

    Added target similarity correlation

--- mri/rsa/rsa_analyse_r_maps.py
+++ mri/rsa/rsa_analyse_r_maps.py
@@ -4,17 +4,23 @@ from os.path import abspath
 from sys import platform
 import pandas as pd
 import itertools
-from nilearn.image import load_img, index_img
+from nilearn.image import load_img, index_img, mean_img
 from nilearn.glm import threshold_stats_img
 import numpy as np
 from nilearn.input_data import NiftiMasker
 from nilearn.masking import _extrapolate_out_mask
 import scipy.stats as stats
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+from nilearn.plotting import plot_glass_brain
+import seaborn as sns
+sns.set_theme(style="darkgrid")
+from multiprocessing import Pool
+
 
 # ------------ File I/O ------------
-# experiment_dir = abspath('/home/achtzehnj/data/timePath/')
-experiment_dir = abspath('/Users/jachtzehn/data/fMRI/timePath')
+experiment_dir = abspath('/home/achtzehnj/data/timePath/')
+# experiment_dir = abspath('/Users/jachtzehn/data/fMRI/timePath')
 
 rsa_dir = opj(experiment_dir, 'derivatives', 'rsa')
 behav_file = opj(experiment_dir, 'derivatives', 'behavioural', 'pse_data_cross_dim_individual_norm.tsv')
@@ -25,21 +31,34 @@ mask_img = load_img(opj(experiment_dir, 'derivatives', 'rsa', 'group_mask_wb_bin
 subjects = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
 conditions = ['time', 'dist', 'dots']
 
-cbar = tqdm(itertools.permutations(conditions, 2), leave=False)
-
 masker = NiftiMasker(mask_img=mask_img, standardize=False, detrend=False,
-                     memory=abspath('/Users/jachtzehn/data/fMRI/nilearn_cache'), memory_level=1)
+                     memory=abspath('/home/achtzehnj/code/nilearn_utilities/cache'), memory_level=1)
 
-for condition_pair in cbar:
-	cbar.set_description('Processing condition pair {} on {}'.format(condition_pair[1], condition_pair[0]))
+# compute mean consistency map
+cons_imgs = []
+for subjectID in subjects:
+	cons_imgs.append(opj(rsa_dir, 'sub-' + str(subjectID).zfill(2),
+	                     'space-MNI152NLin2009cAsym', 'results',
+	                     'sub-' + str(subjectID).zfill(
+		                     2) + '_space-MNI152NLin2009cAsym_wb_cons_values.nii.gz'))
 
+cons_mean_img = mean_img(cons_imgs)
+cons_mean_img.to_filename(opj(rsa_dir, 'mean_consistency.nii.gz'))
+
+#f, ax = plt.subplots(6, 1, figsize=(6, 20))
+#axNr = 0
+
+
+def calc_crossDim(condition_pair):
 	# load crossDim values for all 24 subjects into ndarray
 	crossDim = np.abs(behav_data['pse_diff'][(behav_data.condition_rel == condition_pair[0]) &
-	                                  (behav_data.condition_irrel == condition_pair[1])].values)
+	                                         (behav_data.condition_irrel == condition_pair[1])].values)
 
-	if (condition_pair[0] == 'time' and condition_pair[1] == 'dist') or (condition_pair[1] == 'time' and condition_pair[0] == 'dist'):
+	if (condition_pair[0] == 'time' and condition_pair[1] == 'dist') or (
+			condition_pair[1] == 'time' and condition_pair[0] == 'dist'):
 		dimId = 0
-	elif (condition_pair[0] == 'time' and condition_pair[1] == 'dots') or (condition_pair[1] == 'time' and condition_pair[0] == 'dots'):
+	elif (condition_pair[0] == 'time' and condition_pair[1] == 'dots') or (
+			condition_pair[1] == 'time' and condition_pair[0] == 'dots'):
 		dimId = 1
 	else:
 		dimId = 2
@@ -47,30 +66,36 @@ for condition_pair in cbar:
 	# now load up each participant's RDM map and correlate
 	rdm_dim_data = np.zeros((len(subjects), 64291))
 
+
 	for s, subjectID in enumerate(subjects):
 		rdm_map = load_img(opj(rsa_dir, 'sub-' + str(subjectID).zfill(2), 'space-MNI152NLin2009cAsym', 'results',
-		                       'sub-' + str(subjectID).zfill(2) + '_space-MNI152NLin2009cAsym_zscore_r3_wb_rdm_values.nii.gz'), )
+		                       'sub-' + str(subjectID).zfill(2) + '_space-MNI152NLin2009cAsym_wb_rdm_values.nii.gz'), )
 
 		# extract right 4D volume from nifti file (0 = time vs. space, 1 = time vs. num, 2 = space vs. num
-
 		rdm_map_dim = index_img(rdm_map, dimId)
-
-
 
 		rdm_dim_data[s, :] = masker.fit_transform(rdm_map_dim)
 
 	corr_data = np.zeros((1, 64291))
 	corr_data_p = np.zeros((1, 64291))
 
-	a = rdm_dim_data.shape[1]
+	# load mean consistency map
+	cons_data = masker.fit_transform(cons_mean_img)
+	cons_data_norm = cons_data / np.max(cons_data)
 
 	vbar = tqdm(range(rdm_dim_data.shape[1]), leave=False, mininterval=1)
 	for vx in vbar:
-
-		[vx_corr_r, vx_corr_p] = stats.pearsonr(rdm_dim_data[:, vx], crossDim.T)
+		[vx_corr_r, vx_corr_p] = stats.spearmanr(rdm_dim_data[:, vx], crossDim.T)
 		corr_data[0, vx] = vx_corr_r
-		if vx_corr_p <= 0.01:
+		if vx_corr_p <= 0.05 and cons_data[0, vx] > 0.3 and vx_corr_r < 0:
 			corr_data_p[0, vx] = vx_corr_r
+	corr_data = corr_data * np.abs(cons_data_norm)
+
+	# sns.regplot(x=rdm_dim_data[:, corr_data_p.argmin()], y=crossDim.T, ax=ax[axNr])
+	# ax[axNr].set_xlabel('RDM')
+	# ax[axNr].set_ylabel('CrossDim')
+	# ax[axNr].set_title('rel-{}_irrel-{}'.format(condition_pair[0], condition_pair[1]))
+	# axNr = axNr + 1
 
 	# zscore data
 	corr_data_z = (corr_data - np.mean(corr_data)) / np.std(corr_data)
@@ -81,7 +106,14 @@ for condition_pair in cbar:
 
 	[corr_img_thresh, th] = threshold_stats_img(corr_img_z, cluster_threshold=5, alpha=0.05, height_control='fdr')
 
-	corr_img_thresh.to_filename(opj(rsa_dir, 'corrImg_rel-{}_irrel-{}_thresh.nii.gz'.format(condition_pair[0], condition_pair[1])))
-	corr_img.to_filename(opj(rsa_dir, 'corrImg_rel-{}_irrel-{}.nii.gz'.format(condition_pair[0], condition_pair[1])))
-	corr_img_p.to_filename(opj(rsa_dir, 'corrImg_rel-{}_irrel-{}_p.nii.gz'.format(condition_pair[0], condition_pair[1])))
+	plot_glass_brain(corr_img_p, colorbar=True, cmap='viridis', plot_abs=False, threshold=0, vmax=1, symmetric_cbar=False)
+	plt.savefig(opj(rsa_dir, 'corrImg_rel-{}_irrel-{}_p.pdf'.format(condition_pair[0], condition_pair[1])), format='pdf')
+	plt.close()
 
+	# corr_img_thresh.to_filename(opj(rsa_dir, 'corrImg_rel-{}_irrel-{}_thresh.nii.gz'.format(condition_pair[0], condition_pair[1])))
+	corr_img.to_filename(opj(rsa_dir, 'corrImg_rel-{}_irrel-{}.nii.gz'.format(condition_pair[0], condition_pair[1])))
+	corr_img_p.to_filename(
+		opj(rsa_dir, 'corrImg_rel-{}_irrel-{}_p.nii.gz'.format(condition_pair[0], condition_pair[1])))
+
+pool = Pool(6)
+pool.map(calc_crossDim, itertools.permutations(conditions, 2))
--- mri/rsa/rsa_searchlight_wholeBrain.py
+++ mri/rsa/rsa_searchlight_wholeBrain.py
@@ -15,8 +15,11 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 import numpy as np
 from nilearn.plotting import plot_stat_map
 from nilearn.image.resampling import coord_transform
+from nilearn.image import smooth_img, load_img, new_img_like
 from utilFunctions import create_html_report
 import cPickle
+import itertools
+from sklearn.preprocessing import MinMaxScaler
 
 # ------------ File I/O ------------
 if platform == 'darwin':
@@ -26,6 +29,7 @@ else:
 
 nilearn_dir = opj(experiment_dir, 'derivatives', 'nilearn')
 rsa_dir = opj(experiment_dir, 'derivatives', 'rsa')
+behav_dir = opj(experiment_dir, 'derivatives', 'behavioural')
 
 os.system('mkdir -p %s' % rsa_dir)                 # create the mvpa2 folder for data storage
 
@@ -36,7 +40,7 @@ subjects = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20, 2
 # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]               # all except 15
 conditions = ['time', 'dist', 'dots']
 file_suffix = ''
-sl_radius = 6
+sl_radius = 3
 spaces = ['MNI152NLin2009cAsym']
 
 os.system('clear')                              # clean up terminal
@@ -44,6 +48,7 @@ os.system('clear')                              # clean up terminal
 if file_suffix == '':
     file_suffix = '_'
 
+minmaxscaler = MinMaxScaler(feature_range=(1, 2))
 for space in spaces:
     print('Calculating space: %s' % space)
 
@@ -59,6 +64,19 @@ for space in spaces:
         behav_path = opj(nilearn_dir, subj_str, 'space-' + space, 'betas', subj_str + '_merged_events.tsv')
 
         mask_path = opj(nilearn_dir, 'group_masks', 'space-' + space, 'group_mask_wb_binarized.nii.gz')  # mask filename
+        # -- calculate participants' crossDim matrix
+        pse_diff_data = pd.read_csv(opj(behav_dir, 'pse_data_cross_dim_individual_norm.tsv'), delimiter='\t')
+        crossDim = {}
+        for condition_pair in itertools.permutations(conditions, 2):
+            crossDim['{}_vs_{}'.format(condition_pair[0], condition_pair[1])] = np.mean(np.abs(pse_diff_data['pse_diff'][(((pse_diff_data.condition_rel == condition_pair[0]) &
+                                                             (pse_diff_data.condition_irrel == condition_pair[1])) |
+                                                              ((pse_diff_data.condition_rel == condition_pair[1]) &
+                                                               (pse_diff_data.condition_irrel == condition_pair[0]))) &
+                                                             (pse_diff_data.subject == subj)].values))
+
+        crossDim_matrix = 1 - np.array([crossDim['dist_vs_dots'], crossDim['time_vs_dist'], crossDim['time_vs_dots']])
+        minmaxscaler.fit(crossDim_matrix.reshape(-1, 1))
+        crossDim_matrix_norm = minmaxscaler.transform(crossDim_matrix.reshape(-1, 1))
 
         # read in behav data
         behav_data = pd.read_csv(behav_path, delimiter='\t')
@@ -87,16 +105,48 @@ for space in spaces:
         mtds = mtgs(fmri_data)
         dsm = rsa.PDist(square=False, center_data=True)
 
-        sl = sphere_searchlight(dsm, radius=sl_radius, nproc=24)
+        sl = sphere_searchlight(dsm, radius=sl_radius, nproc=46)
         slres = sl(mtds)
 
         rdm_data_new_order = np.zeros(shape=slres.samples.shape)
         rdm_data_new_order[0, :] = slres.samples[1, :]
         rdm_data_new_order[1, :] = slres.samples[2, :]
         rdm_data_new_order[2, :] = slres.samples[0, :]
-
         rdm_data_new_order = np.nan_to_num(rdm_data_new_order)
-        niimg = map2nifti(fmri_data, rdm_data_new_order)
 
-        niimg.to_filename(opj(result_dir,
+        # now compute RDM consistency by cross-validation with chunks
+        mtcgs = mean_group_sample(['targets', 'chunks'])
+        mtcds = mtcgs(fmri_data)
+        dscm = rsa.PDistConsistency(center_data=True)
+
+        sl_stab = sphere_searchlight(dscm, radius=sl_radius, nproc=46)
+        slres_stab = sl_stab(mtcds)
+        mean_consistency = np.mean(slres_stab, axis=0)
+
+        # now compute similarity with participants' behavioral matrix
+        target_matrix = np.array([crossDim_matrix_norm[0], crossDim_matrix_norm[1], crossDim_matrix_norm[2]]).reshape(-1, )
+        t = slres.samples[:, mean_consistency.argmax()]
+        tdsm = rsa.PDistTargetSimilarity(target_matrix)
+        sl_tdsm = sphere_searchlight(ChainLearner([tdsm, TransposeMapper()]), radius=sl_radius, nproc=46)
+        slres_tdsm = sl_tdsm(mtds)
+
+        #target_data_idx = slres_tdsm.samples[1, :] > 0.05
+        #slres_tdsm.samples[0, target_data_idx] = 0
+
+        niimg_targ = map2nifti(fmri_data, np.nan_to_num(slres_tdsm.samples[0, :]))
+        niimg_targ.to_filename(opj(result_dir,
+                              subj_str + '_space-' + space + file_suffix + 'wb_targ_sim_values.nii.gz'))
+
+        niimg_res = map2nifti(fmri_data, rdm_data_new_order)
+        niimg_res.to_filename(opj(result_dir,
                               subj_str + '_space-' + space + file_suffix + 'wb_rdm_values.nii.gz'))
+
+        niimg_cons = map2nifti(fmri_data, mean_consistency)
+        niimg_cons.to_filename(opj(result_dir,
+                              subj_str + '_space-' + space + file_suffix + 'wb_cons_values.nii.gz'))
+
+        niiimg_cons_tmp = load_img(opj(result_dir,
+                              subj_str + '_space-' + space + file_suffix + 'wb_cons_values.nii.gz'))
+        niiimg_cons_smooth = smooth_img(niiimg_cons_tmp, 6)
+        niiimg_cons_smooth.to_filename(opj(result_dir,
+                              subj_str + '_space-' + space + file_suffix + 'wb_cons_smoothed_values.nii.gz'))
